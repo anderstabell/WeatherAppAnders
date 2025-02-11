7a17f05f43c0536691be7c4fb1fb81d03ce8a321
Anders Tabell <anders.tabell@pianometalstudios.com>
2025-02-10 19:47:18 -0500

    Initial Commit

---
+++ WeatherAppAnders/WeatherAppAndersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppAndersApp.swift
+//  WeatherAppAnders
+//
+//  Created by Anders Tabell on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppAndersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
